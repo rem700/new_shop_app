24fba2c1ed52e6ecbb6365e5b44857e51baee893
rem700 <dmitryzar.rem700@gmail.com>
2020-11-30 20:16:26 +0200

    update

--- lib/screens/otp/otp_screen.dart
+++ lib/screens/otp/otp_screen.dart
@@ -4,6 +4,6 @@ class OtpScreen extends StatelessWidget {
   static String routeName = 'otp';
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold();
   }
 }
